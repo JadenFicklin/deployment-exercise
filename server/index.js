dde01b60a9a804473fc5289c894a0f1077c3f7e9
JadenFicklin <jadendficklin@gmail.com>
2022-01-26 16:34:30 -0700

    troubleshooting

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 app.use("/js", express.static(path.join(__dirname, "public/main.js")));
-app.use("/styles", express.static(path.join(__dirname, "index.css")));
+app.use("/styles", express.static(path.join(__dirname, "/index.css")));
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
